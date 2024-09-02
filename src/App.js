0b1bea99b4773372fb64ec117cc647ccf61afa04
FIFA <156735878+Fifanaja@users.noreply.github.com>
2024-09-02 11:43:09 +0700

    context-header2.js, context-content2.js, context.js
    context-header2.js, context-content2.js, context.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React  from "react"
 import { userContext } from "./context"
 import Header2  from "./context-header2"
-//import { context2 } from "./context-header2"
+import  Content2  from "./context-content2"
 
 export default function App(){
   let [user,setUser]=React.useState('')
@@ -9,7 +9,7 @@ export default function App(){
   return(
     <userContext.Provider value={[user,setUser]}>
     <Header2/>
-  
+    <Content2/>
     </userContext.Provider>
   )
 }
